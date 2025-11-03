7a23306a81f3c28c38ae9067b457f4cd6140cd0c
Aiswaryaraghusp <79993774+Aiswaryaraghusp@users.noreply.github.com>
2025-11-03 18:18:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 chmod 400 KUBE-INFRA-KP.pem
-scp -i "KUBE-INFRA-KP.pem" web/* ubuntu@ec2-13-235-69-97.ap-south-1.compute.amazonaws.com/usr/share/html
-ssh -i "KUBE-INFRA-KP.pem" ubuntu@ec2-13-235-69-97.ap-south-1.compute.amazonaws.com 'sudo systemctl restart nginx'
+mkdir -p ~/.ssh
+echo "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" > ~/.ssh/config
+chmod 600 ~/.ssh/config
+scp -i KUBE-INFRA-KP.pem web/* ubuntu@15.207.89.1:/usr/share/nginx/html/
+ssh -i KUBE-INFRA-KP.pem ubuntu@15.207.89.1'sudo systemctl restart nginx'
